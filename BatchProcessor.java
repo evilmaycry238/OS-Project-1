b69f4c8a18e3793f6ae31d10e21b84baf2a70af3
evilmaycry238 <nmt3664@truman.edu>
2014-09-29 12:14:52 -0500

    Fix for batch 3
    Test worked for every batches except of batch 4

--- BatchProcessor.java
+++ BatchProcessor.java
@@ -3,6 +3,7 @@ package OS.project1;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Process;
 
 public class BatchProcessor 
 {
@@ -12,10 +13,33 @@ public class BatchProcessor
 		BatchParser myBatchParser = new BatchParser();
 		
 		//Again, change this to your own folder
-		File f = new File("work/batch2.xml");
+		File f = new File("work/batch4.xml");
 		batch = myBatchParser.BuildBatch(f);
-		executeBatch(batch);
+		if (!(batch.pipeCmd.id == null || batch.pipeCmd.id.isEmpty()))
+		{
+			System.out.println("Pipe Batch");
+			executePipeBatch(batch);
+		}
+		else
+		{
+			System.out.println("None Pipe Batch");
+			executeBatch(batch);
+		}
 	}
+	
+	static void executePipeBatch(Batch batch) 
+	{
+		for (int i = 0; i < batch.pipeCmd.pipeCmds.size(); i++)
+		{
+			List<String> command = new ArrayList<String>();
+			command.add(batch.pipeCmd.pipeCmds.get(i).path);
+			for(String argi: batch.pipeCmd.pipeCmds.get(i).cmdArgs) 
+			{
+				command.add(argi);
+			}
+		}
+	}
+
 	static void executeBatch(Batch batch) throws ProcessException
 	{
 		for (int i = 0; i < batch.cmdCmds.size(); i++)
@@ -29,40 +53,33 @@ public class BatchProcessor
 			command.add(argi);
 		}
 		
-		//Set the input file
-		String fileIn = batch.cmdCmds.get(i).inID;
-		if (!(fileIn == null || fileIn.isEmpty()))
-		{
-			/* Old methods
-			int intID = Character.getNumericValue(fileIn.charAt(4)) -1;
-			command.add(batch.fileCmds.get(intID).path);
-			*/
-			//New mapping method
-			if( batch.cmdMap.containsKey(fileIn))
-			{
-				command.add(batch.cmdMap.get(fileIn).path);
-			}
-			else
-			{
-				throw new ProcessException("IN FileCommand with id: " + fileIn+"not found");
-			}
-		}
-		
 		ProcessBuilder builder = new ProcessBuilder();
 		builder.command(command);
 		builder.directory(new File(batch.wdCmd.path));
+		
+		//Set the input file
+		String fileIn = batch.cmdCmds.get(i).inID;
+		String input = null;
+		if (!(fileIn == null || fileIn.isEmpty()))
+		{
+			if( batch.cmdMap.containsKey(fileIn))
+			{
+				input = "work/" + batch.cmdMap.get(fileIn).path;
+				System.out.println("this is test: " + input);
+				builder.redirectInput(new File(input));
+			}
+			else
+			{
+				throw new ProcessException("IN FileCommand with id: " + fileIn+ " not found");
+			}
+		}
+		
 		builder.redirectError(new File("error.txt"));
 		
 		//Set the output file
 		String fileOut = batch.cmdCmds.get(i).outID;
 		if (!(fileOut == null || fileOut.isEmpty()))
 		{
-			/* Old methods
-			int outID = Character.getNumericValue(fileOut.charAt(4)) -1;
-			fileOut = "work/" + batch.fileCmds.get(outID).path;
-			builder.redirectOutput(new File(fileOut));
-			*/
-			//New mapping method
 			if( batch.cmdMap.containsKey(fileOut))
 			{
 				fileOut = "work/" + batch.cmdMap.get(fileOut).path;
@@ -70,7 +87,7 @@ public class BatchProcessor
 			}
 			else
 			{
-				throw new ProcessException("Out FileCommand with id: " + fileOut+"not found");
+				throw new ProcessException("Out FileCommand with id: " + fileOut+ " not found");
 			}
 		}
 		
@@ -80,7 +97,6 @@ public class BatchProcessor
 		Process process = builder.start();
 		System.out.println("Command " + i + " executed");
 		process.waitFor();
-		
 		}
 		catch (Exception e) 
 		{
